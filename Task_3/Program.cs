b6f7e0e73b8bd1798980697a2d6d18f772ad3958
Ruslan <89604587433@mail.ru>
2022-09-08 12:11:40 +0300

    Принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N

---
+++ Task_3/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+void CubeNumbers(int namber) //Метод расчитывает куб числа
+{
+    if (namber==0) Console.WriteLine("0");
+    else { if (namber>0)
+              for (int i=1; i<=namber; i++)
+               {
+              Console.WriteLine($"{i} -> {Math.Pow(i,3)}");
+               }
+           if (namber<0)
+               for (int i=1; i>=namber; i--)
+               {
+             Console.WriteLine($"{i} -> {Math.Pow(i,3)}");
+               }
+         }
+}
+Console.Clear();
+Console.WriteLine("Введите число");
+int namber=Convert.ToInt32(Console.ReadLine());
+CubeNumbers(namber);
